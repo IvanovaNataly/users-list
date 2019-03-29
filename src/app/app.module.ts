0213b9627e110b86a032a01441f669dc73a7f0e8
IvanovaNataly <letter4natalia@gmail.com>
2019-03-29 18:42:16 +0300

    login form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { ClarityModule } from '@clr/angular';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -12,22 +12,23 @@ import { ContentAreaComponent } from './components/content-area/content-area.com
 import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    SidebarComponent,
-    ContentAreaComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ClarityModule
-  ],
-  providers: [
-    HttpClient
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent,
+        SidebarComponent,
+        ContentAreaComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ClarityModule,
+        FormsModule
+    ],
+    providers: [
+        HttpClient
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
