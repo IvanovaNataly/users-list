d7339e6ee53b54abc1f3d4e2b8409261ca4fa6e0
IvanovaNataly <letter4natalia@gmail.com>
2019-03-30 16:44:28 +0300

    loginForm completed

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    private agreementNotSubmitted;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onSubmit(loginForm) {
+      if (loginForm.valid) {
+          console.log(loginForm);
+      }
+      else console.log(loginForm.controls.agreement);
+    }
+
+    onBtnSubmit(loginForm) {
+        this.agreementNotSubmitted = !loginForm.controls.agreement.valid;
+    }
 
 }
